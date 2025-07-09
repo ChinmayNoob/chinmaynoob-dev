334b0f04e2a8cdddaf76f174c9dca3b7525ec48e
ChinmayNoob <chinmaysk0408@gmail.com>
2025-07-09 09:31:54 +0530

    updating bio

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Cards } from "@/components/cards"
 import { CommandPalette } from "@/components/command-pallete"
 import TextEffectWithExit from "@/components/headline"
 import { Motion } from "@/components/motion"
+import Link from "next/link"
 
 
 const BLUR_FADE_DELAY = 0.04;
@@ -24,11 +25,15 @@ export default function Home() {
         </div>
       </Motion>
 
-      <section className="prose prose-neutral mt-8 max-w-full dark:prose-invert">
+      <section className="mt-8 max-w-full">
         <Motion>
           <p className="text-base font-normal leading-relaxed">
             I&apos;m a <span className="font-bold text-black dark:text-white">full-stack developer</span> from India, having experience in working with various languages,frameworks and tools.
           </p>
+          <p className="text-base font-normal leading-relaxed">
+            Currently, working on <Link className="font-bold text-black dark:text-white" href="https://acetrumpets.vercel.app" target="_blank">Ace Trumpets UI</Link>. I am open for frontend and full-stack opportunities.
+          </p>
+          <p className="text-base font-normal leading-relaxed">When I am not coding you will find me reading mangas or brainstorming new ideas.</p>
         </Motion>
         <Motion className="flex justify-center">
           <CommandPalette />
