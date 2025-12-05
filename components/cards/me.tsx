30b6006fb8f0261baf35f6b71b6a3b7f1c822fbc
ChinmayNoob <chinmaysk0408@gmail.com>
2025-12-05 16:19:25 +0530

    we got background

--- components/cards/me.tsx
+++ components/cards/me.tsx
@@ -13,7 +13,7 @@ export function MeCard() {
       >
          <Image
             alt="Chinmay Sawant"
-            src="/assets/chinmay-grey.jpg"
+            src="/assets/chinmay-color.png"
             height={300}
             width={300}
             className="h-full object-cover grayscale-[0.35] filter"
